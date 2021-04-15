d4c0b950d629ebeb613bdbf492820503271c0bb0
kevin.francelino <kevin.francelino@redetendencia.com.br>
2021-04-15 08:06:54 -0400

    User card se tornou uma arrow function

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,3 +1,5 @@
+/*
+Esse arquivo deve conter a lista de usuarios , porem esse data temos que puxar do banco de dados da API
 import React, {component} from 'react';
 import {
   View,
@@ -11,10 +13,10 @@ import {
 
 export default function FlatList(){
     return (
-        data = {[1, 2,3,4,5,6,7,8]}, // Lista com os dados dos usuarios 
-        // no caso data vai ser o numero de usuarios do banco 
-        
+        data = {[1, 2,3,4,5,6,7,8]}, // Lista com os dados dos usuarios
+        // no caso data vai ser o numero de usuarios do banco
+
         keyExtractor = {},// Retorna uma chave para ser usada na indexação dos itens da lista
         renderItem = {}, // Função que retorna o componente para renderizar na lista
     )
-}
+}*/
