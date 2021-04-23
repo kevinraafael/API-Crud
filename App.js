0b8c121e2c985b7530055b4825c0411be708364f
kevin.francelino <kevin.francelino@redetendencia.com.br>
2021-04-23 09:56:59 -0400

    Feita alterações nas pastas e arrumado conexao

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from './src/Views/Home';
-import aboutUser from './src/Views/aboutUser';
+import Home from './src/pages/Home';
+import aboutUser from './src/pages/aboutUser';
 
 export default function App() {
   const Stack = createStackNavigator();
